9b3376869b87048b09c497619e8af7f3b9908aa5
Shubhamsinh Rahevar <shubhamrahevar@gmail.com>
2023-02-22 11:51:56 +0530

    Initial Commit

---
+++ Exam2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam2
+//
+//  Created by Shubhamsinh Rahevar on 22/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
